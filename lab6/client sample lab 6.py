0b8521dca72ab0dba97b7d62641280cbc118dd00
divPak <158207537+divPak9876@users.noreply.github.com>
2025-11-19 16:55:55 -0800

    yo

--- lab6/client sample lab 6.py
+++ lab6/client sample lab 6.py
@@ -19,7 +19,6 @@ imageLock = threading.Lock()
 
 IP_ADDRESS = "192.168.1.105" 	# SET THIS TO THE RASPBERRY PI's IP ADDRESS
 RESIZE_SCALE = 2 # try a larger value if your computer is running slow.
-ENABLE_ROBOT_CONNECTION = False
 ENABLE_ROBOT_CONNECTION = True
 
 # You should fill this in with your states
