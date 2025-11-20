e53d8df0f4732723d22272cf8f0187ba6ae393fe
divPak <158207537+divPak9876@users.noreply.github.com>
2025-11-19 16:52:50 -0800

    true

--- lab6/client sample lab 6.py
+++ lab6/client sample lab 6.py
@@ -20,6 +20,7 @@ imageLock = threading.Lock()
 IP_ADDRESS = "192.168.1.105" 	# SET THIS TO THE RASPBERRY PI's IP ADDRESS
 RESIZE_SCALE = 2 # try a larger value if your computer is running slow.
 ENABLE_ROBOT_CONNECTION = False
+ENABLE_ROBOT_CONNECTION = True
 
 # You should fill this in with your states
 class States(enum.Enum):
